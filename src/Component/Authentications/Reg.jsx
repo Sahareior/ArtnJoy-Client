172c1972aca3fae9e2f37bea08d1fd19e36bd6a8
mdAshaduzzaman1 <ashaduzzamannoor19@gmail.com>
2023-06-11 08:13:23 +0600

    password validation added

--- src/Component/Authentications/Reg.jsx
+++ src/Component/Authentications/Reg.jsx
@@ -3,13 +3,14 @@ import { useForm } from 'react-hook-form';
 import useAuth from '../hooks/useAuth';
 
 const Reg = () => {
-  const { register, handleSubmit,reset } = useForm();
-  const { createUser,profile } = useAuth();
+  const { register, handleSubmit, reset, formState: { errors } } = useForm();
+  const { createUser, profile } = useAuth();
 
   const onSubmit = (data, e) => {
     const name = data.name;
     const email = data.email;
     const pass = data.password;
+
     const image = e.target.image.files[0];
     const formData = new FormData();
     formData.append('image', image);
@@ -22,22 +23,21 @@ const Reg = () => {
     })
       .then((res) => res.json())
       .then((imageData) => {
-        const imageUrl = imageData.data.display_url
-          
-    createUser(email, pass)
-    .then((userCredential) => {
-      // Signed in
-     profile(name,imageUrl)
-      console.log(userCredential)
-      reset();
-    })
-    .catch((error) => {
-      const errorMessage = error.message;
-      console.log(errorMessage);
-      // ..
-    });
-      });
+        const imageUrl = imageData.data.display_url;
 
+        createUser(email, pass)
+          .then((userCredential) => {
+            // Signed in
+            profile(name, imageUrl);
+            console.log(userCredential);
+            reset();
+          })
+          .catch((error) => {
+            const errorMessage = error.message;
+            console.log(errorMessage);
+            // ..
+          });
+      });
   };
 
   return (
@@ -69,7 +69,7 @@ const Reg = () => {
                     {...register('image')}
                   />
                 </div>
-                {/*  */}
+
                 <div className="form-control">
                   <label className="label">
                     <span className="label-text">Input Your Name</span>
@@ -81,7 +81,7 @@ const Reg = () => {
                     {...register('name')}
                   />
                 </div>
-                {/*  */}
+
                 <div className="form-control">
                   <label className="label">
                     <span className="label-text">Email</span>
@@ -93,6 +93,7 @@ const Reg = () => {
                     {...register('email')}
                   />
                 </div>
+
                 <div className="form-control">
                   <label className="label">
                     <span className="label-text">Password</span>
@@ -100,10 +101,22 @@ const Reg = () => {
                   <input
                     type="password"
                     placeholder="password"
-                    className="input input-bordered"
-                    {...register('password')}
+                    className={`input input-bordered ${errors.password ? 'input-error' : ''}`}
+                    {...register('password', {
+                      required: 'Password is required',
+                      minLength: {
+                        value: 6,
+                        message: 'Password must be at least 6 characters long'
+                      },
+                      pattern: {
+                        value: /^(?=.*[A-Z])(?=.*[!@#$%^&*])/,
+                        message: 'Password must contain at least one capital letter and one special character'
+                      }
+                    })}
                   />
+                  {errors.password && <p className="text-xs text-error">{errors.password.message}</p>}
                 </div>
+
                 <div className="form-control mt-6">
                   <button type="submit" className="btn btn-primary">
                     Registration
